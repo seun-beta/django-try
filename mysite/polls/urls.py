8143862fe4bc3d9806326907eb131151edd1cccc
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-18 14:18:59 +0100

    first commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>', views.detail, name='detail'),
-    path('<int:question_id>/results', views.results, name='results'),
-    path('<int:question_id>/vote', views.vote, name='vote'),
-    path('cookies', views.cookie_try, name='cookies')
+    path('game/<slug:guess>', views.GameView.as_view()),
+    path('choice/<int:option>', views.ChoiceView.as_view()),
+    path('danger/<str:input>', views.DangerView.as_view()),
+    path('simple', views.simple),
+    path('guess', views.guess),
+    path('special', views.special),
+    path('nuts', views.nuts, name = 'nut'),
+    path('loop', views.loop),
+    path('nested', views.nested),
+    path('nested_objects', views.nested_objects),
+    path('inheritance1', views.Inheritance1.as_view()),
+    path('inheritance2', views.Inheritance2.as_view()),
+    path('urlmap', views.urlmap, name = 'url1')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,28 +1,73 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import get_object_or_404, render
-
-from .models import Question
+from django.http import HttpResponse, response
+from django.http import HttpResponseRedirect
+from django.utils.html import escape
+from django.views import View
+from django.shortcuts import render
+from django.views import View
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    ctx = {'latest_question_list' : latest_question_list,}
-    return render(request, 'polls/index.html', ctx)
+    return HttpResponse('''Hello, world. You're at the polls index.
+    It is great to have you here. This is one of my Django apps''')
 
-def detail(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
-        ctx = {'question', question}
-        return render(request, 'polls/detail.html', ctx )
+class GameView(View):
+    def get(self, request, guess):
+        x = {'guess':int(guess)}
+        return render(request, 'polls/cond.html', x)
 
-def results(request, question_id):
-    response = ("You're looking at the results of question %s.")
-    return HttpResponse(response % question_id )
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question  %s" %question_id)
+class ChoiceView(View):
+    def get(self, request, option):
+        a = {'option':int(option)}
+        return render(request, 'polls/choice.html', a)
 
-def cookie_try(request):
-    print(request.COOKIES)
-    resp = HttpResponse('Welcome to my cookies page')
-    resp.set_cookie('billie', 365)
-    resp.set_cookie('jean', 366, max_age=200)
-    return (resp)
+
+class DangerView(View):
+    def get(self, request, input):
+        b = {'input': input}
+        return render(request, 'polls/danger.html', b)
+
+def simple(request):
+    return render(request, 'polls/simple.html')
+
+def guess(request):
+    context = {'guess':45}
+    return render(request, 'polls/guess.html', context)
+
+def special(request):
+    ctx = { 'special1':12,
+            'special2' : '<b> Bold </b>',
+            'special3' : '<b> Bold </b>'
+    }
+    return render(request, 'polls/special.html', ctx)
+
+def nuts(request):
+    f = ['Apple', 'Orange', 'Pawpaw', 'Kiwi']
+    n = ['cashewnut', 'pistachio', 'groundnut']
+    ctx = {'fruits': f, 'nuts' : n, 'zap' : 42 }
+    return render(request, 'polls/nuts.html', ctx)
+
+def loop(request):
+    ctx = {'loop':[1,2,3,4,5]}
+    return render(request, 'polls/loop.html', ctx)
+
+def nested(request):
+    ctx = { 'outer': {'inner' : 1360 }}
+    return render(request, 'polls/nested.html', ctx)
+
+def nested_objects(request):
+    ctx = {'outer': {'inner': [1,2,3,4,5]}}
+    return render(request, 'polls/nested_objects.html', ctx)
+
+
+class Inheritance1(View):
+    def get(self, request):
+        ctx = {'inheritance1':'This is Inheritance 1'}
+        return render(request, 'polls/inheritance1.html', ctx)
+
+class Inheritance2(View):
+    def get(self, request):
+        ctx = {'inheritance2': 'This is Inheritance 2'} 
+        return render(request, 'polls/inheritance2.html', ctx)
+
+def urlmap(request):
+    return render (request, 'polls/url1.html')
