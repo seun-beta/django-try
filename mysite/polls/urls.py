30aacd623717467a90d18e6b0ba0e7dcc49e4f81
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-19 10:50:49 +0100

    deleted templates from the polls application

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,18 +3,8 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('game/<slug:guess>', views.GameView.as_view()),
-    path('choice/<int:option>', views.ChoiceView.as_view()),
-    path('danger/<str:input>', views.DangerView.as_view()),
-    path('simple', views.simple),
-    path('guess', views.guess),
-    path('special', views.special),
-    path('nuts', views.nuts, name = 'nut'),
-    path('loop', views.loop),
-    path('nested', views.nested),
-    path('nested_objects', views.nested_objects),
-    path('inheritance1', views.Inheritance1.as_view()),
-    path('inheritance2', views.Inheritance2.as_view()),
-    path('urlmap', views.urlmap, name = 'url1')
+    path('get', views.getform, name='get'),
+    path('post', views.postform, name='post'),
+    path('get_try', views.get_try, name='get_try'),
+    path('post_try', views.post_try, name='post_try')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,73 +1,81 @@
 from django.http import HttpResponse, response
 from django.http import HttpResponseRedirect
-from django.utils.html import escape
+from django.utils import html
 from django.views import View
 from django.shortcuts import render
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
-def index(request):
-    return HttpResponse('''Hello, world. You're at the polls index.
-    It is great to have you here. This is one of my Django apps''')
+def dumpdata(label, data):
+    retval = ''
+    if len(data) > 0 :
+        retval = retval + '<p>Incoming '+ label + ' data:</br>\n'
+        for key, value in data.items():
+            retval = retval +  html.escape(key) + '=' + html.escape(value) + '</br>\n'
+        retval += '</p>\n'
 
-class GameView(View):
-    def get(self, request, guess):
-        x = {'guess':int(guess)}
-        return render(request, 'polls/cond.html', x)
+    return retval
 
+def getform(request):
+    response = """
+        <body>
+        <p>
+            GET Request Example
+        </p>
+        <form action="" method="GET">
+            <label for="guess">   Input your guess   </label>
+            <input type="text" name="guess" id="guess">
+            <input type="submit" value="Submit">
+        </form>
+        </body>                      
+    """
+    response = response + dumpdata('GET', request.GET)
+    return HttpResponse(response)
 
-class ChoiceView(View):
-    def get(self, request, option):
-        a = {'option':int(option)}
-        return render(request, 'polls/choice.html', a)
+@csrf_exempt
+def postform(request):
+    response = """
+        <body>
+        <p>
+            POST Request Example
+        </p>
+        <form action="" method="POST">
+            <label for="guess">   Input your guess   </label>
+            <input type="text" name="guess" id="guess">
+            <input type="submit" value="Submit">
+        </form>
+        </body>             
+    """
+    response += dumpdata('POST', request.POST)
+    return HttpResponse(response)
 
+def get_try(request):
+    response = """
+        <body>
+        <p>
+            This is an example of a GET 
+        </p>
+        <form action="" method="GET">
+            <label for="guess">   Input your number:   </label></br>
+            <input type="text" name="guess" id="guess">
+            <input type="submit" value="Submit">
+        </form>
+        </body>             
+    """
+    return HttpResponse(response)
 
-class DangerView(View):
-    def get(self, request, input):
-        b = {'input': input}
-        return render(request, 'polls/danger.html', b)
-
-def simple(request):
-    return render(request, 'polls/simple.html')
-
-def guess(request):
-    context = {'guess':45}
-    return render(request, 'polls/guess.html', context)
-
-def special(request):
-    ctx = { 'special1':12,
-            'special2' : '<b> Bold </b>',
-            'special3' : '<b> Bold </b>'
-    }
-    return render(request, 'polls/special.html', ctx)
-
-def nuts(request):
-    f = ['Apple', 'Orange', 'Pawpaw', 'Kiwi']
-    n = ['cashewnut', 'pistachio', 'groundnut']
-    ctx = {'fruits': f, 'nuts' : n, 'zap' : 42 }
-    return render(request, 'polls/nuts.html', ctx)
-
-def loop(request):
-    ctx = {'loop':[1,2,3,4,5]}
-    return render(request, 'polls/loop.html', ctx)
-
-def nested(request):
-    ctx = { 'outer': {'inner' : 1360 }}
-    return render(request, 'polls/nested.html', ctx)
-
-def nested_objects(request):
-    ctx = {'outer': {'inner': [1,2,3,4,5]}}
-    return render(request, 'polls/nested_objects.html', ctx)
-
-
-class Inheritance1(View):
-    def get(self, request):
-        ctx = {'inheritance1':'This is Inheritance 1'}
-        return render(request, 'polls/inheritance1.html', ctx)
-
-class Inheritance2(View):
-    def get(self, request):
-        ctx = {'inheritance2': 'This is Inheritance 2'} 
-        return render(request, 'polls/inheritance2.html', ctx)
-
-def urlmap(request):
-    return render (request, 'polls/url1.html')
+@csrf_exempt
+def post_try(request):
+    response = """
+        <body>
+        <p>
+            This is an example of a POST 
+        </p>
+        <form action="" method="POST">
+            <label for="guess">   Input your guess:   </label></br>
+            <input type="text" name="guess" id="guess">
+            <input type="submit" value="Submit">
+        </form>
+        </body>             
+    """
+    return HttpResponse(response)
