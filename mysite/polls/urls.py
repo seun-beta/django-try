1e84abd1e80aa35ff5286d93dea46aa8d5f50471
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-25 22:43:09 +0100

    refactored code to ensure PEP 8 compliance

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('get_try', views.get_try, name='get_try'),
     path('post_try', views.post_try, name='post_try'),
     path('guess_try/<str:guess>', views.guess_try, name='guess_try'),
-    path('guess/<int:guess>', views.GuessView.as_view(), name='guess')
+    path('guess/<int:guess>', views.GuessView.as_view(), name='guess'),
+    path('cookie', views.cookie, name='cookie')
 ]
